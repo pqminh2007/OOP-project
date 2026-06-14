b3582772de527771a5f1867b7bc30a9de901ecd5
Minh <phungquangminh070@gmail.com>
2026-06-14 16:18:12 +0700

    feat: add Room.h and Person.h headers

---
+++ HotelManagementSystem/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/CMakeFiles/HotelManagementSystem.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "HotelManagementSystem_autogen/timestamp" "custom" "HotelManagementSystem_autogen/deps"
+  "C:/Users/HN/source/repos/OOP-project/HotelManagementSystem/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/HotelManagementSystem_autogen/mocs_compilation.cpp" "CMakeFiles/HotelManagementSystem.dir/HotelManagementSystem_autogen/mocs_compilation.cpp.obj" "gcc" "CMakeFiles/HotelManagementSystem.dir/HotelManagementSystem_autogen/mocs_compilation.cpp.obj.d"
+  "C:/Users/HN/source/repos/OOP-project/HotelManagementSystem/main.cpp" "CMakeFiles/HotelManagementSystem.dir/main.cpp.obj" "gcc" "CMakeFiles/HotelManagementSystem.dir/main.cpp.obj.d"
+  "C:/Users/HN/source/repos/OOP-project/HotelManagementSystem/mainwindow.cpp" "CMakeFiles/HotelManagementSystem.dir/mainwindow.cpp.obj" "gcc" "CMakeFiles/HotelManagementSystem.dir/mainwindow.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ HotelManagementSystem/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/CMakeFiles/HotelManagementSystem_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HotelManagementSystem_autogen"
+  "HotelManagementSystem_autogen/include/ui_mainwindow.h"
+  "HotelManagementSystem_autogen/mocs_compilation.cpp"
+  "HotelManagementSystem_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/HotelManagementSystem_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
